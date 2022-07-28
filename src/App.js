09047d7aca88794fd441b3be7270eec8ac4cc9f9
Nabeil <nabeillemar@gmail.com>
2022-07-28 11:38:42 -0700

    created Ingredients path

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './containers/Home'
 import Navigation from './components/Navigation';
+import IngredientsContainer from './containers/IngredientsContainer';
+import IngredientsList from './containers/IngredientsList';
 
 function App() {
   return (
     <Router>
-      <div className="App">
       <Navigation />
+      <div className="App">
         <Switch>
           <Route exact path='/' component={Home}/>
           <Route exact path='/categories' component={CategoriesContainer} />
+          <Route exact path='/ingredients' component={IngredientsContainer} />
+          <Route exact path='/ingredients/:id' component={IngredientsList} />
+          
         </Switch>
       </div>
     </Router>
