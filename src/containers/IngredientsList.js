c97c57541838d24f0c32a0e9cc2e267799d4136f
Nabeil <nabeillemar@gmail.com>
2022-07-29 13:43:11 -0700

    created Ingredients path

---
+++ src/containers/IngredientsList.js
@@ -0,0 +1,15 @@
+// view a list of meals with the given ingrident 
+import React, { Component } from 'react'
+import MealsContainer from "./MealsContainer"
+
+class IngredientsContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h2> The Meals with {this.props.match.params.id}</h2>
+      </div>
+    )
+  }
+}
+
+export default IngredientsContainer
