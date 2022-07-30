9a9b8d42eb39adf1c41e23e1bf09c365de5ac699
Nabeil <nabeillemar@gmail.com>
2022-07-30 09:53:59 -0700

    created Ingredients link button

--- src/components/Ingredient.js
+++ src/components/Ingredient.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 const Ingredient = (props) => {
   return (
     <div>
-        <Link to={`/ingreidents/${props.ingredient.split(" ").join("_")}`}>
+        <Link to={`/ingredients/${props.ingredient.split(" ").join("_")}`}>
         <button>{props.ingredient}</button>
           </Link>
     </div>
