8ce540288d416832781d8ed240b2604911d9924c
Nabeil <nabeillemar@gmail.com>
2022-07-30 18:56:25 -0700

    created IngredientsList props

--- src/components/Ingredient.js
+++ src/components/Ingredient.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 const Ingredient = (props) => {
   return (
     <div>
-        <Link to={`/ingredients/${props.ingredient.split(" ").join("_")}`}>
+        <Link to={`/ingredients/${props.ingredient.split(" ").join("_")}`}> 
         <button>{props.ingredient}</button>
           </Link>
     </div>
@@ -12,3 +12,5 @@ const Ingredient = (props) => {
 }
 
 export default Ingredient
+// We are splitting the name of the ingredient and adding the "-" here 
+// chicken break would be chicken_breast
--- src/containers/IngredientsList.js
+++ src/containers/IngredientsList.js
@@ -6,7 +6,9 @@ class IngredientsContainer extends Component {
   render() {
     return (
       <div>
-        <h2> The Meals with {this.props.match.params.id}</h2>
+        <h2> The Meals with {this.props.match.params.id.split("_").join(" ")}</h2>
+        <MealsContainer searchTerm={this.props.match.params.id.split("_").join(" ")} searchType="i" banner="" history={this.props} />
+        {console.log(this.props.match.id)}
       </div>
     )
   }
